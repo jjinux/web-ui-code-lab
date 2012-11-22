68aa9915e1e8bf298edaebc058e0a38922fc6a5c
Shannon -jj Behrens <jjinux@gmail.com>
2012-11-21 16:46:58 -0800

    Style

--- finished/lib/server_utils.dart
+++ finished/lib/server_utils.dart
@@ -3,13 +3,14 @@ library server_utils;
 import 'dart:isolate';
 
 time(msg, callback()) {
-  var sw = new Stopwatch()..start();
+  var sw = new Stopwatch();
+  sw.start();
   callback();
   sw.stop();
   print('Timing for $msg: ${sw.elapsedMicroseconds} us');
 }
 
-// runs the callback on the event loop at the next opportunity
+/// Run the callback on the event loop at the next opportunity.
 queue(callback()) {
   new Timer(0, (t) => callback());
 }
