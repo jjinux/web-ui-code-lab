4986a3cfd8d5c62a0eab146add97f4fe86e5659d
Shannon -jj Behrens <jjinux@gmail.com>
2012-11-23 13:29:41 -0800

    Integrated the other Web Components into the application

--- finished/web/application.dart
+++ finished/web/application.dart
@@ -5,11 +5,15 @@ import 'dart:isolate' show Timer;
 import 'package:web_components/web_components.dart';
 import 'chat_connection.dart';
 import 'out/chat_window.html.dart';
+import 'out/username_input.html.dart';
+import 'out/message_input.html.dart';
 
 class Application {
   const connectionUrl = "ws://127.0.0.1:1337/ws";
   ChatConnection chatConnection;
   ChatWindowComponent chatWindow;
+  UsernameInputComponent usernameInput;
+  MessageInputComponent messageInput;
 
   Application() {
     // The Web Components aren't ready immediately in index.html's main.
@@ -17,10 +21,11 @@ class Application {
       
       // xtag is how you get to the Dart object.
       chatWindow = query("#chat-window").xtag;
+      usernameInput = query("#username-input").xtag;
+      messageInput = query("#message-input").xtag;
 
       chatWindow.displayNotice("web component connected");
       chatConnection = new ChatConnection(this, connectionUrl);
-    });
-    
+    });   
   }
 }
