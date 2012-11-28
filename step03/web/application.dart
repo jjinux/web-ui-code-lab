a436519ec2102994c2e0b4159b561eefd3abb6e2
Shannon -jj Behrens <jjinux@gmail.com>
2012-11-29 07:07:53 +0800

    Ported changes made per code review to other steps:
        commit 1fccbacf35552adf0ba777409a0e5242d3f1af3f
        Don't use an Application class to wrap stuff
    
        commit 2167c314090b37e5c384270950866b8ba1c65178
        Declare versions explicitly
    
        commit f6b51b10d7d52e0121e98536030a0df1d2fa4c89
        Add an explicit call to dispatch per John Messerly

--- step03/web/application.dart
+++ step03/web/application.dart
@@ -5,22 +5,16 @@ import 'dart:isolate' show Timer;
 import 'package:web_components/web_components.dart';
 import 'out/chat_window.html.dart';
 
-class Application {
-  ChatWindowComponent chatWindow;
-  
-  Application() {
-    // The Web Components aren't ready immediately in index.html's main.
-    new Timer(0, (timer) {
-      
-      // xtag is how you get to the Dart object.
-      chatWindow = query("#chat-window").xtag;
-      chatWindow.displayNotice("web component connected");
-    });   
-  }
-}
+ChatWindowComponent chatWindow;
 
-Application _app;
-get app {
-  if (_app == null) _app = new Application();
-  return _app;
+init() {
+  // The Web Components aren't ready immediately in index.html's main.
+  new Timer(0, (timer) {
+    
+    // xtag is how you get to the Dart object.
+    chatWindow = query("#chat-window").xtag;
+    chatWindow.displayNotice("web component connected");
+    
+    dispatch();
+  });   
 }
