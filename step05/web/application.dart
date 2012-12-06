c7620f34b2ae0cf80969003358eb8e0b75cbd5d5
Shannon -jj Behrens <jjinux@gmail.com>
2012-12-06 15:37:08 -0800

    Added a step05 to make things more declarative

---
+++ step05/web/application.dart
@@ -0,0 +1,31 @@
+library application;
+
+import 'dart:html';
+import 'dart:isolate' show Timer;
+import 'package:web_components/web_components.dart';
+import 'chat_connection.dart';
+import 'out/chat_window.html.dart';
+import 'out/username_input.html.dart';
+import 'out/message_input.html.dart';
+
+const connectionUrl = "ws://127.0.0.1:1337/ws";
+ChatConnection chatConnection;
+ChatWindowComponent chatWindow;
+UsernameInputComponent usernameInput;
+MessageInputComponent messageInput;
+
+init() {
+  // The Web Components aren't ready immediately in index.html's main.
+  new Timer(0, (timer) {
+    
+    // xtag is how you get to the Dart object.
+    chatWindow = query("#chat-window").xtag;
+    usernameInput = query("#username-input").xtag;
+    messageInput = query("#message-input").xtag;
+
+    chatWindow.displayNotice("web component connected");
+    chatConnection = new ChatConnection(connectionUrl);
+    
+    dispatch();
+  });   
+}
